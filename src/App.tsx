2d77b9d83f07ae21b854c1fb6397652b466bfb1b
hellonathapon <hellonathapon@gmail.com>
2021-03-25 08:27:07 +0700

    Config webpack and add custom type for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './styles.css'
+import tsLogoPath from './ts-logo-128.png';
+import reactLogoPath from './react-logo.svg';
 
 const App = () => {
     return (
         <div>
+            <img src={tsLogoPath} alt="TS logo"/>
+            <img src={reactLogoPath} className="svg" alt="React logo"/>
             <h1>Hello <code>React, Typescript, Bebal</code> and <code>Webpack</code></h1>
         </div>
     )
---
+++ src/custom.d.ts
@@ -0,0 +1,5 @@
+// well sometimes libs do not come along with its @type
+// to fix TS warning and error @type of target lib should be declare manually.
+
+declare module '*png'
+declare module '*svg'
